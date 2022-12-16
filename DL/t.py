acd6b360fb8232057ebb95df85e5de02225e7c79
Lana <lana690543@gmail.com>
2022-12-16 11:13:27 +0800

    註解

--- DL/t.py
+++ DL/t.py
@@ -16,15 +16,12 @@ import random
 
 data_dir = pathlib.Path('D:/lanac/deepLearning/DL/flowers')
 
-"""
-    參數設定
-"""
-BATCH_SIZE = 32
-IMG_SIZE = 128
-AUTOTUNE = tf.data.AUTOTUNE
-epochs = 15
+#參數設定
+BATCH_SIZE = 32 #一次抓取的量
+IMG_SIZE = 128 
+epochs = 15 #步數
 
-
+#讀imgs
 train_data = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
 #   color_mode='grayscale',
@@ -41,30 +38,34 @@ test_data = tf.keras.preprocessing.image_dataset_from_directory(
   batch_size=BATCH_SIZE
 )  
 
-
 class_names = train_data.class_names #['daisy','dandelion','rose','sunflower','tulip']
 
-#標準化
+
+#---性能優化---------
+AUTOTUNE = tf.data.AUTOTUNE
+#.cache() : 第一次load後，會將img保存memory 
+#.prefetch()：可以先做預處理
 train_data = train_data.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 test_data = test_data.cache().prefetch(buffer_size=AUTOTUNE)
 
+
 #########################################
-#---------建模---------
+#---建模---------
 model = Sequential([
-    Rescaling(1./255, input_shape=(IMG_SIZE, IMG_SIZE, 3)),#標準化
-    
+    Rescaling(1.0/255 ,input_shape=(IMG_SIZE, IMG_SIZE, 3)), #標準化(因0-255組)
+    #第一層
     Conv2D(16, (3,3), padding='same', activation='relu'),
     MaxPooling2D((2,2)),
     Dropout(0.2),
-
+    #第二層
     Conv2D(32, (3,3), padding='same', activation='relu'),
     MaxPooling2D((2,2)),
     Dropout(0.2),
-
+    #第三層
     Conv2D(64, (3,3), padding='same', activation='relu'),
     MaxPooling2D((2,2)),
     Dropout(0.2),
-    
+    #攤平、連接
     Flatten(),
     Dense(128,   activation='relu'),
     Dense(5, activation='softmax'),
@@ -72,9 +73,8 @@ model = Sequential([
 
 #---------訓練----------
 model.compile(
-    optimizer='adam',
-    # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    loss='sparse_categorical_crossentropy',
+    optimizer='adam', #adam:學習速度是動態的
+    loss='sparse_categorical_crossentropy', #分類時常用
     metrics=['accuracy']
 )
 
@@ -87,7 +87,7 @@ history = model.fit(
 scores = model.evaluate(test_data)  
 
 
-#抓lables
+#---抓lables----------
 pred_labels = np.array([])
 test_labels = np.array([])
 for x, y in test_data:
@@ -96,7 +96,7 @@ for x, y in test_data:
     test_labels = np.concatenate([test_labels, y.numpy()])
 
 
-#->>>>>>>>>>>>>>>> PLT <<<<<<<<<<<<<<<<<<<<<<<<
+#->>>>>>>>>>>>>>>> 畫圖區PLT <<<<<<<<<<<<<<<<<<<<<<<<
 
 acc = history.history['accuracy'] #訓練集正確率
 val_acc = history.history['val_accuracy'] #測試集正確率
@@ -111,12 +111,12 @@ plt.subplot(1, 2, 1)
 plt.plot(epochs_range, acc, label='Training Accuracy')
 plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+plt.title('Accuracy')
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
 plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
+plt.title('Loss')
 
 #2.混淆矩陣
 CM = confusion_matrix(test_labels, pred_labels)
@@ -140,6 +140,7 @@ for images, labels in test_data.take(1): #拿一bacth的量(32)
         plt.subplot(4, 4, index)
         plt.tight_layout()
         plt.axis('off')
+        #顯示結果
         text = class_names[labels.numpy()[i]] 
         text = text + '  (Correct)' if labels.numpy()[i] == pred_labels_slice[i] else text + '   (Incorrect)'  
         plt.title(text)
