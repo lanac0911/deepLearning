809ac577e1a804608a306831060e363fcdc95c05
Lana <lana690543@gmail.com>
2022-12-18 08:24:09 +0800

    HC數據平滑OK

--- Knapsack/compoents.py
+++ Knapsack/compoents.py
@@ -15,9 +15,9 @@ def UpdateNowState(blist, w, v):
 def initialState():
     global best_state
     pickBound = math.pow(2, int(varibles.objNums)) #upperbound: 2^15
-    inti_w = init_v = 0
+    inti_w = 0; init_v = 0
     while(1):
-        initNum = format(random.randrange(int(pickBound/2) , pickBound), 'b') #範圍: 1 - 2^15
+        initNum = format(random.randrange(int(pickBound/2) , pickBound), 'b') #範圍: (2^15/2) - 2^15
         blist = binToList(initNum) #拆成list
         (w, v) = calTotalWandV(blist)  #計算weight & value
         
@@ -53,23 +53,69 @@ def calTotalWandV(blist):
     return(tempWeight, tempValue)
 
 
+#----------找鄰居的方法(HC)----------
+def delOrAdd_HeavyOrLight(list, mode):
+    # mode=0做增加 , mode=1做刪減
+    hasNeig = False; d = dict()
+    indices = [i for i, x in enumerate(list) if x == mode]
+    if(len(indices) >= 1): #刪一個輕的
+        idx = random.choice(indices) #隨機挑一個1
+        list[idx] = int(not mode)#刪
+        hasNeig = True
+    d['flag'] = hasNeig 
+    d['new_oper_list'] = list 
+    return d
+
+
+
 #>>>>>>>>>>> HILL CLIMBL <<<<<<<<<<
 def HillClimbing():
-    test_state = now_state.copy()
-    now_v = now_state['value']
-    #生成 neighbors *15 (翻轉每兩位元)
-    for index, pick in enumerate(now_state['blist']): #遍歷每個位元
+    # for index, pick in enumerate(now_state['blist']): #遍歷每個位元
     # for i in range(0,varibles.objNums-1 ,1): #遍歷每兩位元
-        new_list = now_state['blist'].copy()
-        new_list[index] = int(not pick )
-        (w, v) = calTotalWandV(new_list)
-        if w <= varibles.capcity: #合法
-            if v > now_v: #新better than 舊 => 交換
-                test_state['blist'] = new_list
-                test_state['weight'] = w
-                now_v = v
+    new_list = now_state['blist'].copy()
+    now_v = now_state['value']
+    front = int(varibles.objNums / 2); 
+    front_list = new_list[:front] #前半
+    end_list = new_list[front:] #後半
+    flag1 = False; flag2 = False #加/減有無找到鄰居
 
-    UpdateNowState(test_state['blist'], test_state['weight'], now_v)  
+   # 找鄰居：
+   #    將binary數分成前半、後半(權重輕、權重重)(已由小 → 大排序))
+   #    〔加一個，減一個〕操作都要從【前半、後半】中選一個(每次由隨機機率決定)
+   #    若剛好前半、後半都無法再加減
+   #    則隨機取一位元做翻轉
+
+    doAdd = random.random() #前半、後半誰要做加
+    doDel = random.random() #前半、後半誰要做減
+    #加------
+    if doAdd > 0.5: #重的+1
+        res = delOrAdd_HeavyOrLight(end_list, 0)  #add Heavy
+        end_list = res['new_oper_list']
+        flag1 = res['flag']
+    else: #輕的+1
+        res = delOrAdd_HeavyOrLight(front_list, 0) #add Light
+        front_list = res['new_oper_list']
+        flag1 = res['flag']
+    #減-------
+    if doDel > 0.5: #重的-1
+        res = delOrAdd_HeavyOrLight(end_list, 1) #del Heavy
+        end_list = res['new_oper_list']
+        flag2 = res['flag']
+    else: #輕的-1
+        res = delOrAdd_HeavyOrLight(front_list, 1) #del Light
+        front_list = res['new_oper_list']
+        flag2 = res['flag']
+    new_list = front_list + end_list
+    #都失敗-----
+    if (flag1 or flag2) == 0: #隨機翻轉一位元
+        idx = random.randrange(0, varibles.objNums)
+        new_list[new_list] = (not new_list[idx])
+
+    (w, v) = calTotalWandV(new_list)
+    if w <= varibles.capcity: #合法
+        if v > now_v: #新better than 舊 => 交換
+            UpdateNowState(new_list, w, v)  
+    
     return (now_state)
 
 
@@ -94,7 +140,6 @@ def SimulationAnnealing():
                     best_state['blist'] = test_list
                     best_state['weight'] = test_w
                     best_state['value'] = test_v
-                    flag = True    
             #now更新
             if test_v > now_v : #優於當前解 -> 更新
                 UpdateNowState(test_list, test_w, test_v)
--- Knapsack/main.py
+++ Knapsack/main.py
@@ -17,6 +17,7 @@ def loadData():
         f = open(path, 'r')
         if path == paths[0] :
             varibles.capcity = int(f.read())
+            # varibles.capcity = int(varibles.capcity / 2)
         elif path == paths[1]:
             varibles.weights = loadDataLoop(f)
         else:
@@ -54,9 +55,9 @@ if __name__ == '__main__':
     ans = {}
     while(1):
         inputn = input("\n――――選擇模式 ―――― \n 1：Hill Climbing       ｜\n 2：Simulation Annealing｜ \n―――――――――――――\n")
-        choice = int(inputn)
-        if(choice == 1): ans = HillClimb(choice); break
-        elif(choice == 2): ans = SA(choice); break
+        choice = inputn
+        if(choice == "1"): ans = HillClimb(); break
+        elif(choice == "2"): ans = SA(); break
         else: print("錯誤，請重新輸入")
  #print結果
     print("\n=========================================\n")
@@ -68,12 +69,11 @@ if __name__ == '__main__':
     print(f"* 結果：\n value：{ans['value']}\n weight：{ans['weight']}\n pick：{ans['blist']}")
     print("=========================================\n")
   #畫圖 40-60  1458
-    n = ans['value']
-    yrange = (n-15, n+1)
+    # yrange = (min(history),)
     plt.figure()
     plt.ylabel('values')
     plt.xlabel('times')
     plt.plot(history)        
-    plt.ylim(yrange)
+    # plt.ylim(yrange)
     plt.show()
 
